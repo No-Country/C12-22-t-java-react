7ad142f40a7fb2afc53173bf294c9e158ca8a170
eltinchoo <mattpaganini07@hotmail.com>
2023-07-06 22:16:43 -0400

    some changes in the navbar

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -6,44 +6,41 @@ const Navbar = () => {
 
   const searhbar = () => [console.log(product)];
   return (
-    <nav className="bg-gray-800 h-36 flex justify-beetwen items-center">
+    <nav className="bg-gray-800 h-24 flex justify-beetwen items-center">
       
       {/* first container */}
-      <div className="flex flex-col justify-center items-center ml-14">
+      <div className="flex  justify-center items-center ml-6">
         <NavLink href="#home" to="/" as={Link}>
-          <i className="fa-brands fa-apple text-gray-300 hover:text-white text-5xl mt-2"></i>
+          <i className="fa-brands fa-apple text-gray-300 hover:text-white text-4xl mt-2 ml-3 mr-5 mb-2"></i>
         </NavLink>
 
-        <NavLink href="#home" to="/" as={Link} className="mt-6">
-          <span className="text-gray-300 hover:text-white text-xl">Home</span>
+        <NavLink href="#home" to="/" as={Link}>
+          <span className="text-gray-300 hover:text-white text-xl mr-5">Home</span>
         </NavLink>
-      </div>
      
-     {/* second container */}
-      <div className="flex gap-4 mt-20 ml-12">
         <NavLink href="#mac" to="/mac" as={Link} >
-        <span className="text-gray-300 hover:text-white text-xl">Mac</span>  
+          <span className="text-gray-300 hover:text-white text-xl mr-5">Mac</span>  
         </NavLink>
 
         <NavLink href="#iPad" to="/ipad" as={Link} className="text-white">
-        <span className="text-gray-300 hover:text-white text-xl ml-6">iPad</span> 
+        <span className="text-gray-300 hover:text-white text-xl mr-5">iPad</span> 
         </NavLink>
 
         <NavLink href="#iPhone" to="/iphone" as={Link} className="text-white ">
-        <span className="text-gray-300 hover:text-white text-xl ml-6">iPhone</span> 
+        <span className="text-gray-300 hover:text-white text-xl ">iPhone</span> 
         </NavLink>
       </div>
       
       {/* input container */}
-      <div className="flex justify-start items-center gap-1.5 ml-52">
+      <div className="flex justify-start items-center gap-1.5 ml-40">
         <input
-          placeholder="buscar producto"
+          placeholder="Buscar..."
           className="rounded-md p-3 outline-red-900 w-80"
           value={product}
           onChange={e => setProduct(e.target.value)}
         ></input>
         <button
-          className="text-white bg-gray-500 rounded-md p-3 ml-2 hover:bg-green-700"
+          className="text-white bg-gray-500 rounded-md p-3 ml-2 hover:bg-gray-300 hover:text-gray-800 "
           onClick={searhbar}
         >
           <i className="fa-solid fa-magnifying-glass p-0.5"></i>
