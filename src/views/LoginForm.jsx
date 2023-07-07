3c78dfc7e791c2ec9719fa6d3778c4e7a66a810d
Yeiner Parra <yeinerparrabernal@gmail.com>
2023-07-06 20:50:05 -0500

    Component LoginForm update style

--- src/views/LoginForm.jsx
+++ src/views/LoginForm.jsx
@@ -5,7 +5,7 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     //validar y enviar los datos al servidor
     console.log(email, password);
@@ -14,30 +14,38 @@ function Login() {
   return (
     <div className="login bg-gray-400 min-h-screen flex items-center justify-center">
       <div className="bg-white shadow-lg rounded-lg p-4 w-80">
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">Iniciar sesión</h1>
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          Iniciar sesión
+        </h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-600 text-sm font-medium mb-2">
+            <label
+              htmlFor="email"
+              className="block text-gray-600 text-sm font-medium mb-2"
+            >
               Email
             </label>
             <input
               type="email"
               id="email"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={e => setEmail(e.target.value)}
               required
               className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-black-500 focus:ring focus:ring-black-200"
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-600 text-sm font-medium mb-2">
+            <label
+              htmlFor="password"
+              className="block text-gray-600 text-sm font-medium mb-2"
+            >
               Contraseña
             </label>
             <input
               type="password"
               id="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={e => setPassword(e.target.value)}
               required
               className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-black-500 focus:ring focus:ring-black-200"
             />
@@ -49,10 +57,15 @@ function Login() {
             Entrar
           </button>
           <div className="mt-4">
-          <NavLink href="#createuser" to="/createuser" as={Link} className="text-center text-sm text-gray-600 underline hover:text-gray-900">
-            ¿No tienes cuenta? Regístrate aquí
-          </NavLink>
-        </div>
+            <NavLink
+              href="#createuser"
+              to="/createuser"
+              as={Link}
+              className="text-center text-sm text-gray-600 underline hover:text-gray-900"
+            >
+              ¿No tienes cuenta? Regístrate aquí
+            </NavLink>
+          </div>
         </form>
       </div>
     </div>
