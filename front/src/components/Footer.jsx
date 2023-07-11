b51f4a8a9622e2dc38e3e889ba9900e1b9fbd98c
PabloCastro <pablocastrod7@gmail.com>
2023-07-10 22:57:52 -0300

    Starting Iphone view

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -6,10 +6,9 @@ const Footer = () => {
     <footer className="bg-gray-200">
       <div className="container mx-auto px-6 py-3 flex align-center flex-col md:justify-around md:flex-row">
         <div className="flex flex-col md:w-72 ">
-          <div className='flex align-center'>
-          <i className="fa-brands fa-apple text-gray-400 hover:text-white text-3xl pr-4"></i>
-          <h3 className="text-3xl font-bold mb-3">Apple Store</h3>
-          
+          <div className="flex align-center">
+            <i className="fa-brands fa-apple text-gray-400 hover:text-white text-3xl pr-4"></i>
+            <h3 className="text-3xl font-bold mb-3">Apple Store</h3>
           </div>
           <p className="mb-1 italic">
             Somos una pequeña empresa que vendemos productos de Apple.
@@ -20,17 +19,19 @@ const Footer = () => {
           <a href="#" className="hover:text-slate-800">
             Términos y condiciones
           </a>
-          <Link to='/Contact' className="hover:text-slate-800">
+          <Link to="/Contact" className="hover:text-slate-800">
             Trabajá con nosotros
           </Link>
-          <Link to='/Contact' className="hover:text-slate-800">
+          <Link to="/Contact" className="hover:text-slate-800">
             Contacto
           </Link>
         </div>
         <div className="flex flex-col mb-6 md:mb-0">
-          <h3 className="text-3xl font-bold mt-6 mb-3 md:mt-0">Medios de pago</h3>
+          <h3 className="text-3xl font-bold mt-6 mb-3 md:mt-0">
+            Medios de pago
+          </h3>
           <div className="flex justify-center flex-row gap-4">
-            <div className='text-center'>
+            <div className="text-center">
               <img
                 src="https://d26lpennugtm8s.cloudfront.net/stores/001/014/432/rte/logo-mercadopago.png"
                 alt="Visa"
@@ -38,7 +39,7 @@ const Footer = () => {
               />
               <span className="text-xs text-center">Mercado Pago</span>
             </div>
-            <div className='text-center'>
+            <div className="text-center">
               <img
                 src="https://d26lpennugtm8s.cloudfront.net/stores/001/014/432/rte/logo-mercadopago.png"
                 alt="Mastercard"
@@ -46,7 +47,7 @@ const Footer = () => {
               />
               <span className="text-xs text-center">Efectivo</span>
             </div>
-            <div className='text-center'>
+            <div className="text-center">
               <img
                 src="https://d26lpennugtm8s.cloudfront.net/stores/001/014/432/rte/logo-mercadopago.png"
                 alt="American Express"
@@ -54,7 +55,7 @@ const Footer = () => {
               />
               <span className="text-xs text-center">Transferencia</span>
             </div>
-            <div className='text-center'>
+            <div className="text-center">
               <img
                 src="https://d26lpennugtm8s.cloudfront.net/stores/001/014/432/rte/logo-mercadopago.png"
                 alt="Mercado Pago"
