253ae4ffbcef98c12bb7f5f86fc1f4c910c87746
eltinchoo <mattpaganini07@hotmail.com>
2023-07-14 00:53:21 -0400

    macbook view in progress

--- front/src/views/Mac.jsx
+++ front/src/views/Mac.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Mac = () => {
+    const [macbook, setMacbook] = useState([]);
+
+    useEffect(() => {
+        const urlMacbook = 'https://appleclon.onrender.com/product/mac'
+      axios.get(urlMacbook)
+            .then(res => {
+                console.log(res.data)
+                const macbook = res.data.data.model[0].productos
+                setMacbook(macbook)
+            })
+            .catch(error => {
+                console.error(error);
+              });
+    }, []);
+  console.log(macbook);
+
+  const addProduct = () => {
+    alert("Macbook añadida al carrito")
+  }
     return (
-        <div>
-         Macbook component   
+        <div className=' text-center w-full mx-auto h-full'>
+        <section className='flex flex-col justify-center align-self '>
+        <h1 className='font-semibold mt-6 text-4xl'>Macbook</h1>
+        <p className='mt-6'>{macbook[0]?.descripcion}</p>
+        </section>
+        <h2>Macbok Pro</h2>
+        <section className='flex justify-center'>
+        <ul>
+            {
+                macbook.map(mac => (
+                    <li className='border-solid  border border-gray-600 p-10 rounded-xl mt-6 mb-6'>
+                        <img src={mac.imagen1}/>
+                        <div className='flex justify-center align-self mt-5'>
+                       <p>USD <b >{mac.precio}</b></p>
+                       <i class="fa-solid fa-cart-plus ml-3 mt-1 hover:text-sky-400 " onClick={() => addProduct()}></i>
+                       </div>
+                    </li>
+                    
+                ))
+            }
+        </ul>
+        </section>
         </div>
     );
 };
