20f6385613c03112efa9caa4b37346024213c0ec
MarvinArch <marvinejo@gmail.com>
2023-07-19 16:05:17 -0600

    cambios en descripcion

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,5 +1,11 @@
 CREATE TABLE Categoria(
-    character_id LONG AUTO_INCREMENT,
     nombre_categoria VARCHAR(50) NOT NULL,
         CONSTRAINT PK_CATEGORIA PRIMARY KEY(nombre_categoria)
+);
+
+CREATE TABLE categoria_producto(
+    categoria_producto LONG AUTO_INCREMENT,
+    nombre_producto VARCHAR(50) NOT NULL,
+    descripcion TEXT,
+        CONSTRAINT PK_CATEGORIA_PRODUCTO PRIMARY KEY(categoria_producto)
 );
