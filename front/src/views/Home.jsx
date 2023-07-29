7c4c111f04b8742cd2ba4e1bc057be70e37b288e
Yeiner Parra <yeinerparrabernal@gmail.com>
2023-07-29 14:48:20 -0500

    update home component

--- front/src/views/Home.jsx
+++ front/src/views/Home.jsx
@@ -28,11 +28,12 @@ const Home = () => {
             Alto rendimiento a un precio sorprendente.
           </h1>
           <p className="py-6 text-sm md:text-xl lg:text-2xl md:text-start">
-            El iPhone 13 ya está aquí. Haz fotos increíbles, disfruta de las
+            El iPhone 14 Pro Max ya está aquí. Haz fotos increíbles, disfruta de las
             funciones de seguridad mejor valoradas y aprovecha toda la potencia
             de Apple.{' '}
           </p>
-          <Link to={`/detail/${allProducts[1]?.productos[2]?.categoriaProductoId}`}
+          <Link
+            to={`/detail/${allProducts[1]?.productos[2]?.categoriaProductoId}`}
             className="py-2 bg-white  border-solid border-2 border-gray-700 hover:bg-gray-700 hover:text-white
                       shadow  w-full sm:w-auto px-4 rounded-md text-lg
                       transition ease-in duration-out m-auto text-center"
@@ -46,11 +47,12 @@ const Home = () => {
               <React.Fragment key={category.nombreCategoria}>
                 {category.productos.map(
                   (product, index) =>
-                    index === 2 && (
+                    category.nombreCategoria === 'Iphone' &&
+                    index === 3 && (
                       <div key={product.nombreProducto}>
                         <img
                           className="w-3/5 mx-auto"
-                          src={product.imagen1}
+                          src={product.imagen3}
                           alt={product.nombreProducto}
                         />
                         <h3 className="text-center text-2xl font-bold">
@@ -78,7 +80,6 @@ const Home = () => {
                   key={product.nombreProducto}
                   className="bg-white items-center justify-center border rounded-lg p-4 flex flex-col"
                 >
-                  
                   <img
                     className="h-40 w-40"
                     src={product.imagen1}
@@ -99,7 +100,6 @@ const Home = () => {
                     Ver Mas
                   </Link>
                 </div>
-                
               ))}
             </React.Fragment>
           ))}
